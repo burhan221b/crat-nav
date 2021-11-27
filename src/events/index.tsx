1b2c7207c0b28e80b2f975f120da37a991d264fb
Burhan <burhan221b@gmail.com>
2021-11-26 16:13:40 -0800

    Fixed layout version and scroll

--- src/events/index.tsx
+++ src/events/index.tsx
@@ -1,12 +1,13 @@
+//! V2
 const openSideNav = () => {
     document.querySelector('#nav-top-menu-bar')?.addEventListener('click', () => {
-        document.querySelector('#nav-side')?.classList.add('active')
+        document.querySelector('#NavSide')?.classList.add('active')
         document.querySelectorAll('.overlay')?.forEach(overlay => overlay.classList.add('active'))
     })
 }
 
 const closeSideNav = () => {
-    document.querySelector('#nav-side-menu-close')?.addEventListener('click', () => {
+    document.querySelector('#NavSideCloseBtn')?.addEventListener('click', () => {
         removeSideNavActive()
     })
 }
@@ -21,25 +22,73 @@ const removeOverlay = () => {
 }
 
 const removeSideNavActive = () => {
-    console.log("remove active")
-    document.querySelector('#nav-side')?.classList.remove('active')
+    document.querySelector('#NavSide')?.classList.remove('active')
     document.querySelectorAll('.overlay')?.forEach(overlay => overlay.classList.remove('active'))
 }
 
 const openSideNavTopic = (topic: string) => {
-    console.log("CLICK topic", topic)
     document.querySelector(`#${topic}`)?.classList.add('chosen-wing');
-    document.querySelector('#SIDE-MAIN-MENU')?.classList.add('left-wing');
-    document.querySelector('#SIDE-MAIN-MENU')?.classList.remove('chosen-main-wing');
+    document.querySelector('#NavSideMainMenu')?.classList.add('left-wing');
+    // document.querySelector('#NavSideMainMenu')?.classList.remove('chosen-main-wing');
+    document.querySelector('#NavSideMainMenu')?.classList.remove('right-wing');
 
 }
 
 const closeSideNavTopic = (topic: string) => {
     document.querySelector(`#${topic}`)?.classList.add('right-wing');
     document.querySelector(`#${topic}`)?.classList.remove('chosen-wing');
-    document.querySelector('#SIDE-MAIN-MENU')?.classList.remove('left-wing');
-    document.querySelector('#SIDE-MAIN-MENU')?.classList.add('chosen-main-wing');
+    document.querySelector('#NavSideMainMenu')?.classList.remove('left-wing');
+    // document.querySelector('#NavSideMainMenu')?.classList.add('chosen-main-wing');
 
 }
 
 export { openSideNav, closeSideNav, removeOverlay, removeSideNavActive, openSideNavTopic, closeSideNavTopic }
+
+
+//! V1
+
+// const openSideNav = () => {
+//     document.querySelector('#nav-top-menu-bar')?.addEventListener('click', () => {
+//         document.querySelector('#nav-side')?.classList.add('active')
+//         document.querySelectorAll('.overlay')?.forEach(overlay => overlay.classList.add('active'))
+//     })
+// }
+
+// const closeSideNav = () => {
+//     document.querySelector('#nav-side-menu-close')?.addEventListener('click', () => {
+//         removeSideNavActive()
+//     })
+// }
+
+// const removeOverlay = () => {
+//     const overlays = document.querySelectorAll('.overlay')
+//     overlays?.forEach(overlay => {
+//         overlay.addEventListener('click', () => {
+//             removeSideNavActive()
+//         })
+//     })
+// }
+
+// const removeSideNavActive = () => {
+//     console.log("remove active")
+//     document.querySelector('#nav-side')?.classList.remove('active')
+//     document.querySelectorAll('.overlay')?.forEach(overlay => overlay.classList.remove('active'))
+// }
+
+// const openSideNavTopic = (topic: string) => {
+//     console.log("CLICK topic", topic)
+//     document.querySelector(`#${topic}`)?.classList.add('chosen-wing');
+//     document.querySelector('#SIDE-MAIN-MENU')?.classList.add('left-wing');
+//     document.querySelector('#SIDE-MAIN-MENU')?.classList.remove('chosen-main-wing');
+
+// }
+
+// const closeSideNavTopic = (topic: string) => {
+//     document.querySelector(`#${topic}`)?.classList.add('right-wing');
+//     document.querySelector(`#${topic}`)?.classList.remove('chosen-wing');
+//     document.querySelector('#SIDE-MAIN-MENU')?.classList.remove('left-wing');
+//     document.querySelector('#SIDE-MAIN-MENU')?.classList.add('chosen-main-wing');
+
+// }
+
+// export { openSideNav, closeSideNav, removeOverlay, removeSideNavActive, openSideNavTopic, closeSideNavTopic }
